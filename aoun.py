cf8a69548525273dbb2f126e9636ec3a9741d8be
treyzania <treyzania@gmail.com>
2017-07-27 15:30:59 -0400

    Fixed authentication and everything.

--- aoun.py
+++ aoun.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import os
 import sys
@@ -7,22 +7,22 @@ import discord
 import asyncio
 
 args = sys.argv
-user_id = args[0]
-server_id = args[1]
-channel_id = args[2]
+user_id = args[1]
+server_id = args[2]
+channel_name = args[3]
+invite = args[4]
 
 client = discord.Client()
 
 config = {
-    'intro_channel': 'introductions'
     'groups': {
-        '\'first': '1st Year',
-        '\'second': '2nd Year',
-        '\'third': '3rd Year',
-        '\'fourth': '4th Year',
-        '\'fifth': '5th Year',
-        '\'grad': 'Grad Student',
-        '\'alum': 'Alum'
+        '\'first': '1st year',
+        '\'second': '2nd year',
+        '\'third': '3rd year',
+        '\'fourth': '4th year',
+        '\'fifth': '5th year',
+        '\'grad': 'grad-student',
+        '\'alum': 'alum'
     }
 }
 
@@ -39,28 +39,42 @@ def find_role(name):
         if r.name == name:
             return r
 
+def get_channel(name):
+    serv = client.get_server(server_id)
+    if serv is None:
+        client.accept_invite(invite)
+        client.get_server(server_id)
+    for c in serv.channels:
+        if c.name == name:
+            return c
+
 @client.event
 async def on_ready():
     print('Username:', client.user.name)
     print('UID:', client.user.id)
+    print('Invite: https://discordapp.com/oauth2/authorize?client_id={}&scope=bot'.format(client.user.id))
     print('READY TO ROCK AND ROLL BABY')
+    chan = get_channel(channel_name)
+    client.send_message(chan, 'Hello, World!')
 
 @client.event
 async def on_member_join(member):
     print('New user', client.user.name)
-    serv = client.get_server(server_id)
-    chan = server.get_channel(channel_id)
+    chan = get_channel(channel_name)
     client.send_message(chan, get_join_message(member))
 
 @client.event
 async def on_message(message):
-    if message.channel.id != channel_id:
+    if message.channel.name != channel_name:
         return
     for k, v in config.groups.items():
+        role = find_role(v)
+        if role in message.user.roles:
+            return
         if message.content.contains(k):
             print('Trying to assign role', v, 'to', message.user.name, '...')
-            role = find_role(v)
             client.add_roles(message.user, role)
             print('OK!')
 
+print('Got user ID', user_id)
 client.run(user_id)
