4742fb91c2b953cf1367029af4b89909379a088f
treyzania <treyzania@gmail.com>
2017-09-06 21:41:16 -0400

    Made the overall flow work, still needs to fix announcing old posts.

--- aounnit.py
+++ aounnit.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import datetime
 
 import discord
 import asyncio
@@ -16,8 +17,8 @@ rclientsecret = os.environ['rclientsecret']
 rusername = 'AounBot' # I shouldn't hardcode this, but whatever.
 rpassword = os.environ['rpassword']
 ruseragent = 'AounBot by /u/Treyzania for /r/NEU'
-rsubreddit = 'NEU' # Probably shouldn't hardcode this either.
-rsleep = 60 # Ever 60 seconds.  Also shouldn't hardcode this.
+rsubreddit = 'Treyzania' # Probably shouldn't hardcode this either.
+rsleep = 10 # Ever 60 seconds.  Also shouldn't hardcode this.
 dclientid = os.environ['dclientid']
 dserverid = os.environ['dserverid']
 dchannelname = os.environ['dchannelname']
@@ -25,6 +26,33 @@ dinvite = os.environ['dinvite']
 
 dc = discord.Client()
 dchannel = None
+
+reddit = praw.Reddit( \
+    client_id = rclientid, \
+    client_secret = rclientsecret, \
+    username = rusername, \
+    password = rpassword, \
+    user_agent = ruseragent)
+
+lastcheck = datetime.datetime.utcnow()
+
+@asyncio.coroutine
+async def reddit_loop(chan):
+    global lastcheck
+    await dc.wait_until_ready()
+    neusub = reddit.subreddit(rsubreddit)
+    seen = []
+    while True:
+        print('Checking subreddits...')
+        recent = neusub.new(limit = 5)
+        for p in recent:
+            if datetime.datetime.utcfromtimestamp(p.created) > lastcheck: # How the fuck do I fix this?
+                print('New post:', str(p))
+                await dc.send_message(chan, '➤ **New reddit post:** ' + p.shortlink)
+            else:
+                print('Already submitted post:', str(p))
+        lastcheck = datetime.datetime.utcnow()
+        await asyncio.sleep(rsleep)
 
 @dc.event
 async def on_ready():
@@ -34,38 +62,7 @@ async def on_ready():
     for s in dc.servers:
         for c in s.channels:
             if c.name == dchannelname:
-                dchannel = c
-                await dc.send_message(c, 'i read it on reddit')
-
-reddit = praw.Reddit( \
-    client_id=rclientid, \
-    client_secret=rclientsecret, \
-    username=rusername, \
-    password1=rpassword, \
-    user_agent=ruseragent)
-
-#print('Reddit username:', reddit.user.me())
-#print('Reddit auth code:', reddit.auth.authorize())
-
-async def reddit_loop():
-    dc.wait_until_ready()
-    time.sleep(10)
-    neusub = reddit.subreddit(rsubreddit)
-    seen = []
-    while True:
-        print('Checking', rsubreddit, '...')
-        recent = neusub.new(limit=5)
-        for p in recent:
-            if p in seen:
-                break
-            seen.append(p)
-            if dc is not None:
-                print('found new post', str(p))
-                await dc.send_message(dchannel, '>>> New reddit post: ' + p.shortlink)
-            else:
-                print('found new post,', str(p), 'but we can\'t post it yet')
-        time.sleep(rsleep)
+                dc.loop.create_task(reddit_loop(c))
 
 print('Got user ID:', dclientid)
-dc.loop.create_task(reddit_loop())
 dc.run(dclientid)
